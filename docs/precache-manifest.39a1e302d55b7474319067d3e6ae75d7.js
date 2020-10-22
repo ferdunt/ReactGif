6b24035b6d8e372a88c041f1dd12e7851b2a575a
Fernando Guisbert <guisbertfernando@gmail.com>
2020-10-22 12:04:56 -0400

    docs add

---
+++ docs/precache-manifest.39a1e302d55b7474319067d3e6ae75d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5adf49be6ef7deb7de43d89f12d433",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f6146454a9493b6ada",
+    "url": "/static/css/main.579218fe.chunk.css"
+  },
+  {
+    "revision": "e915424eeb4a1675bf19",
+    "url": "/static/js/2.7e8e1ec7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.7e8e1ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f6146454a9493b6ada",
+    "url": "/static/js/main.dd41fc4b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
